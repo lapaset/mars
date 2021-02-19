c44b8cd4de31f09ffad8eb45cbd013d146f3fade
llahti <llahti@student.hive.fi>
2021-02-19 13:51:04 +0200

    Init client with jest, eslint and prettier

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <h1>Mars</h1>
+    </header>
+  </div>
+)
+
+export default App
---
+++ client/src/App.test.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import App from './App'
+
+test('App renders', () => {
+  render(<App />)
+})
+
+test()
+
+/* test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+}); */
+
+/* dtc0lIsqFGs7ZFH2hbwFdYMbktMAzoiaaadC09Qy */
