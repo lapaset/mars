2cfbe259a9b9bbfd09ee75980f1dec2cb5b7b09b
llahti <llahti@student.hive.fi>
2021-03-25 17:40:14 +0200

    Get photos from Api with infinite scroll

--- client/src/App.js
+++ client/src/App.js
@@ -1,84 +1,122 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import axios from 'axios'
 import PropTypes from 'prop-types'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import { ThemeProvider } from '@material-ui/core/styles'
 import withWidth, { isWidthUp } from '@material-ui/core/withWidth'
-import { Typography, GridList, GridListTile } from '@material-ui/core/'
+import { Container, GridList, GridListTile } from '@material-ui/core/'
 import { apiKey } from './secret.json'
+import Header from './components/Header'
 import Photo from './components/Photo'
 import theme from './styles/theme'
 
-const baseUrl = 'https://api.nasa.gov/mars-photos/api/v1/'
-const earthDate = '2021-2-20'
+const baseUrl = 'https://api.nasa.gov/mars-photos/api/v1'
+const photosPerPage = 25
+const rover = 'curiosity'
 
 const App = (props) => {
   const [photos, setPhotos] = useState(null)
+  const [sol, setSol] = useState(null)
+  const getGridListCols = () => (isWidthUp('sm', props.width) ? 6 : 3)
+  const lastPhotoRef = useRef()
+  const pageRef = useRef(1)
+  const loadingRef = useRef(false)
 
   useEffect(async () => {
-    const data = await axios.get(
-      `${baseUrl}rovers/curiosity/photos?earth_date=${earthDate}&api_key=${apiKey}`
-    )
-    // console.log('🚀 ~ file: App.js ~ line 16 ~ useEffect ~ data ', data)
-    setPhotos(data.data.photos)
+    const meta = await axios.get(`${baseUrl}/manifests/${rover}?api_key=${apiKey}`)
+    setSol(meta.data.photo_manifest.max_sol)
   }, [])
 
-  const getGridListCols = () => {
-    if (isWidthUp('xl', props.width)) return 11
-    if (isWidthUp('lg', props.width)) return 9
-    if (isWidthUp('md', props.width)) return 7
-    if (isWidthUp('sm', props.width)) return 5
-    return 3
-  }
+  useEffect(async () => {
+    if (!sol) return
+    const { data } = await axios.get(
+      `${baseUrl}/rovers/${rover}/photos?sol=${sol}&page=${pageRef.current}&api_key=${apiKey}`
+    )
+    pageRef.current = data.photos.length < photosPerPage ? null : pageRef.current + 1
+    setPhotos(data.photos)
+  }, [sol])
 
-  const getGridTileSize = (id) => {
-    const getSize = (a, b) => {
-      if (id % a === 0) return 3
-      if (id % b === 0) return 2
-      return 1
+  useEffect(() => {
+    const handleScroll = async () => {
+      if (
+        loadingRef.current ||
+        !sol ||
+        !photos ||
+        !pageRef.current ||
+        !lastPhotoRef ||
+        lastPhotoRef.current.getBoundingClientRect().top > window.innerHeight
+      )
+        return
+
+      try {
+        loadingRef.current = true
+        const { data } = await axios.get(
+          `${baseUrl}/rovers/${rover}/photos?sol=${sol}&page=${pageRef.current}&api_key=${apiKey}`
+        )
+        if (data.photos.length > 0) setPhotos(photos.concat(data.photos))
+        pageRef.current = data.photos.length < photosPerPage ? null : pageRef.current + 1
+        loadingRef.current = false
+      } catch (e) {
+        // eslint-disable-next-line no-console
+        console.log('Error loading more photos', e)
+      }
     }
 
-    if (isWidthUp('xl', props.width)) return getSize(3, 2)
-    if (isWidthUp('lg', props.width)) return getSize(11, 2)
-    if (isWidthUp('md', props.width)) return getSize(8, 3)
-    if (isWidthUp('sm', props.width)) return getSize(7, 4)
+    window.addEventListener('scroll', handleScroll, { passive: true })
+
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [sol, photos])
+
+  const getGridTileSize = (id) => {
+    const getSizeBig = () => {
+      if (id % 7 === 0) return getGridListCols()
+      return 2
+    }
+
+    if (isWidthUp('sm', props.width)) return getSizeBig()
     return id % 3 === 0 ? 2 : 1
   }
 
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <div className="App">
-        <header className="App-header">
-          <Typography variant="h1" align="right" gutterBottom>
-            Mars
-          </Typography>
-          <Typography variant="body1" align="right" gutterBottom>
-            Exploring and colonizing Mars can bring us new scientific understanding of climate
-            change, of how planet-wide processes can make a warm and wet world into a barren
-            landscape. By exploring and understanding Mars, we may gain key insights into the past
-            and future of our own world.
-          </Typography>
-          <Typography variant="body2" align="right" gutterBottom>
-            Buzz Aldrin
-          </Typography>
-        </header>
+      <Container className="App" maxWidth="sm">
+        {sol && <Header sol={sol} />}
         <main>
           {photos && (
-            <GridList cellHeight={160} className={theme.gridList} cols={getGridListCols()}>
-              {photos.map((p) => (
-                <GridListTile key={p.id} cols={getGridTileSize(p.id)} rows={getGridTileSize(p.id)}>
-                  <Photo
-                    src={p.img_src}
-                    alt={`${p.rover.name} ${p.camera.full_name} ${p.earth_date}`}
-                    className="MuiGridListTile-imgFullHeight"
-                  />
-                </GridListTile>
-              ))}
+            <GridList cellHeight={100} className={theme.gridList} cols={getGridListCols()}>
+              {photos.map((p, i) =>
+                i === photos.length - 1 ? (
+                  <GridListTile
+                    key={p.id}
+                    cols={getGridTileSize(p.id)}
+                    rows={getGridTileSize(p.id)}
+                  >
+                    <div ref={lastPhotoRef} />
+                    <Photo
+                      src={p.img_src}
+                      alt={`${p.rover.name} ${p.camera.full_name} ${p.earth_date}`}
+                      className="MuiGridListTile-imgFullHeight"
+                    />
+                  </GridListTile>
+                ) : (
+                  <GridListTile
+                    key={p.id}
+                    cols={getGridTileSize(p.id)}
+                    rows={getGridTileSize(p.id)}
+                  >
+                    <Photo
+                      src={p.img_src}
+                      alt={`${p.rover.name} ${p.camera.full_name} ${p.earth_date}`}
+                      className="MuiGridListTile-imgFullHeight"
+                    />
+                  </GridListTile>
+                )
+              )}
             </GridList>
           )}
         </main>
-      </div>
+      </Container>
     </ThemeProvider>
   )
 }
